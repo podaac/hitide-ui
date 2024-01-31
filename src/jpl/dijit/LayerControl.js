d6ba260f1cb4d36bfbf711c6ff6fb8d7179e2dd7
jbyrne <jimmy.byrne@jpl.nasa.gov>
2024-01-31 12:22:15 -0800

    issue/hitide-ui-42: revert layer control file back

--- src/jpl/dijit/LayerControl.js
+++ src/jpl/dijit/LayerControl.js
@@ -256,9 +256,9 @@ define([
                 var granuleName = obj.granuleObj["Granule-Name"];
                 var variableName = this.variableId;
                 var imageFilename = calculateImageFilename(granuleName, variableName);
-                var relatedUrls = obj.granuleObj.umm.RelatedUrls;
 
-                
+
+                var relatedUrls = obj.granuleObj.umm.RelatedUrls;
                 var objectURL
                 for(var i=0; i < relatedUrls.length; i++){
                     objectURL = relatedUrls[i]['URL'];

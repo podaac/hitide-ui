e4e8b54cc3a1d264d991efe49e2e5cb767579a40
Simon Liu <69875423+sliu008@users.noreply.github.com>
2024-02-20 14:48:32 -0800

    Feature/fix swot alignment (#55)
    * update ummt sync github action
    
    * update so we run cron at 1 am pst
    
    * update again
    
    * add in global grid from hitide configurations to fix swot 2.0 alignment
    
    * update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 ### Added
+- Added global_grid configuration check to make a global extent for images.
 ### Changed
 ### Removed
 ### Fixed
--- src/hitideConfig.js
+++ src/hitideConfig.js
@@ -1,7 +1,7 @@
 var hitideProfileOrigin = "https://hitide.profile.podaac.uat.earthdatacloud.nasa.gov/hitide/api";
 
 window.hitideConfig = {
-    paletteService: "https://hitide.podaac.uat.earthdatacloud.nasa.gov/palettes",
+    paletteService: "https://hitide.podaac.earthdatacloud.nasa.gov/palettes",
 
     cmrDatasetSearchService: hitideProfileOrigin + "/cmr/search/collections.json?tool_name=HiTIDE",
     cmrGranuleSearchService: hitideProfileOrigin + "/cmr/search/granules.umm_json",

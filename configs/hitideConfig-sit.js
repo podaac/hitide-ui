9ce75c3e3b4db5ca5cbdebf546dd916bcd24cea9
Simon Liu <simon.liu@jpl.nasa.gov>
2025-11-05 13:08:51 -0800

    /deploy sit

--- configs/hitideConfig-sit.js
+++ configs/hitideConfig-sit.js
@@ -24,5 +24,5 @@ window.hitideConfig = {
     datasetSearchServiceItemsPerPage: 200,
     maxGranulesPerDownload: 1000,
     googleTagManagerId: "GTM-M5D83V6",
-    earthDataAppClientId: "B8wVj-BFID-XXhLjIiKS5g"
+    earthDataAppClientId: "WNuqfswx4L5jT_4y572N5w"
 };
